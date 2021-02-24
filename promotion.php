97f8d1825f01b65f2b8acaaad8bf3ccdaf9d1e15
pattarapong41 <79399021+pattarapong41@users.noreply.github.com>
2021-02-24 14:11:40 +0700

    Add files via upload

---
+++ promotion.php
@@ -0,0 +1,69 @@
+<?php include "./layouts/header2.php"; ?>
+
+
+
+
+      <body>
+	     <br/>
+		 <br/>
+	       <center>
+	       <table border="0" width="850" height="420">
+		      <tr>
+			     <td height="320"><img src="assets/images/Atrium Grilled River Prawn.jpg" width="980"></td>
+			  </tr>
+			  <tr>
+			     <td><center><h5>บุฟเฟ่ต์ กุ้งแม่น้ำ พม่า @ Atrium, the Landmark Bangkok</h5></center><br/>
+				              บุฟเฟ่ต์ กุ้งแม่น้ำ พม่า @ Atrium, the Landmark Bangkok โปรโมชั่น ที่นำเอากุ้งแม่น้ำอิรวดี 
+					 มาอยู่ในไลน์บุฟเฟ่ต์ ให้เราสั่งกินได้แบบไม่อั้น ไม่จำกัดจำนวน โดยเฉพาะคนที่ชอบกินกุ้งเผา รับรองว่า ฟินขั้นสุด 
+					 แถมยังมีโปรโมชั่น มา 4 จ่าย 2 เท่ากับมาสี่คน ลด 50% กันเลย 
+				 </td>
+			  </tr>
+			  <tr>
+			     <td height="320"><img src="assets/images/promotion-buffet-cake-sweet-spell-250.jpg" width="980"></td>
+			  </tr>
+			  <tr>
+			     <td><center><h5>Promotion Buffet Cake w/Candies Sweet Heart อร่อยไม่อั้นแค่ 250 บาท </h5></center><br/>
+				              โด่งดังเป็น Talk Of The Pro กันไปกับบุฟเฟ่ต์เค้กโปรโมชั่นโดนๆในช่วงวันแห่งความรักกับ โปรโมชั่นวาเลนไทน์แสนเปลี่ยว ทานเค้กคนเดียวดีกว่า 
+				              ถึงจะหมดวันแห่งความรักไปแล้ว ก้ยังมีโปรโมชั่น Buffet Cake ที่ร้าน Candies Sweet Heart ให้อร่อยกันต่อเนื่องตามปกติเช่นเคย 
+				    จะมาคนเดียวหรือมาเป็นคู่ก็อร่อยได้เหมือนกัน^^อร่อยคุ้มกับ Promotion Buffet Cake อร่อยไม่อั้นแค่ 250 บาท Net.
+					ได้ทุกวัน ในราคาปกติ 250 บาท/ท่าน +เครื่องดื่ม Refill 1 แก้ว
+				 </td>
+			  </tr>
+			  <tr>
+			     <td height="320"><img src="assets/images/cbuffet.jpg" width="980"></td>
+			  </tr>
+			  <tr>
+			     <td><center><h5>พรีเมี่ยมบุฟเฟ่ต์แห่งปี Copper Buffet สุดจัดย่านปิ่นเกล้า แน่นทั้งคุณภาพและปริมาณ นึกถึงบุฟเฟ่ต์นานาชาติทีไรต้องคิดถึงที่นี่</h5></center><br/>
+				      พรีเมี่ยมบุฟเฟ่ต์แห่งปี Copper Buffet สุดจัดย่านปิ่นเกล้า แน่นทั้งคุณภาพและปริมาณ นึกถึงบุฟเฟ่ต์นานาชาติทีไรต้องคิดถึงที่นี่ ยิ่งที่นี่จะมีเมนูพิเศษมาให้เลือกตลอด
+				     ความพรีเมี่ยมคือสุดจริงๆ เมนูหลากหลาย แต่ละเมนูน่าตื่นตาตื่นใจ มีแทบทุกอย่าง  ทั้งเมนูไทย ญี่ปุ่น ฝรั่ง ครบหมดเลย 
+				 </td>
+			  </tr>
+			  <tr>
+			     <td height="320"><img src="assets/images/mt.jpg" width="980"></td>
+			  </tr>
+			  <tr>
+			     <td><center><h5>งานแต่งเย็น แถมฟรีงานเช้า Graph Hotel x See-through Wedding</h5></center><br/>
+				     Package รวมงานแต่งเย็น 150 คน พร้อมอาหาร รับฟรีงานเช้าพร้อม Coffee break 30 ท่าน และพิเศษแถมฟรีชุด Exclusive จาก See-through
+				 </td>
+			  </tr>
+		   </table>
+		   </center>
+		   
+		   
+		   <!-- Optional JavaScript; choose one of the two! -->
+
+           <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
+<script src="https://code.jquery.com/jquery-3.5.1.js" integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc="
+        crossorigin="anonymous"></script>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns"
+        crossorigin="anonymous"></script>
+
+<script src="https://cdn.datatables.net/1.10.23/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/1.10.23/js/dataTables.bootstrap4.min.js"></script>
+
+
+<script src="//cdn.jsdelivr.net/npm/sweetalert2@10"></script>
+	  </body>
+</html>
