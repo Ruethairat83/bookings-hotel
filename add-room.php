0e47ad0a9f1308b0399a95fd9933cf4e9ca85403
Biw-Natthathida <79396115+Biw-Natthathida@users.noreply.github.com>
2021-02-24 14:38:52 +0700

    Add files via upload

---
+++ add-room.php
@@ -0,0 +1,44 @@
+<?php include "./layouts/header.php"; ?>
+
+
+<div class="container py-4">
+    <div class="row mt-4 mb-4">
+        <div class="col-12">
+            <h4>เพิ่มประเภทห้อง</h4>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-12 col-md-12">
+            <form action="./functions/insert-room.php" method="post" enctype="multipart/form-data">
+                <input type="hidden" id="hotel_id" name="hotel_id" value="<?php echo $_GET["hotel_id"]; ?>">
+                <input type="hidden" id="room_type_id" name="room_type_id" value="<?php echo $_GET["room_type_id"]; ?>">
+                <div class="form-group row">
+                    <label for="room_no" class="col-sm-2 col-form-label">เลขห้อง</label>
+                    <div class="col-sm-10">
+                        <input type="text" class="form-control" id="room_no" name="room_no">
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label for="file" class="col-sm-2 col-form-label">รูปภาพ</label>
+                    <div class="col-sm-10">
+                        <input type="file" class="form-control" id="file" name="file">
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <div class="col-sm-10">
+                        <button type="submit" class="btn btn-primary">บันทึก</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+
+
+<?php include "./layouts/footer.php"; ?>
+
+
+<script>
+    $(document).ready(function () {
+    });
+</script>
